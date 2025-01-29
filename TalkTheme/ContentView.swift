77d92d3586201948671e32742cde3a58da2d705e
yuimu02 <yuikomu1002@gmail.com>
2025-01-29 19:23:05 +0900

    Initial Commit

---
+++ TalkTheme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TalkTheme
+//
+//  Created by Yuiko Muroyama on 2025/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TalkTheme/TalkThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkThemeApp.swift
+//  TalkTheme
+//
+//  Created by Yuiko Muroyama on 2025/01/29.
+//
+
+import SwiftUI
+
+@main
+struct TalkThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
